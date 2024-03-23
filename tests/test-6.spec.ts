162db0e84322126b9fbdd445127e715c2df0c17d
Priya Manthry <priya.manthry@gmail.com>
2024-03-23 06:09:03 +0530

    Made little label change

--- tests/test-6.spec.ts
+++ tests/test-6.spec.ts
@@ -1,10 +1,9 @@
 import { test, expect } from '@playwright/test';
-
 test('test', async ({ page }) => {
   await page.goto('https://doorknock-web-git-dev-doorknock-old.vercel.app/');
   await page.getByRole('button', { name: 'Sign in' }).click();
   await page.getByRole('button', { name: 'Sign in with Google Continue' }).click();
   await page.getByLabel('Email or phone').click();
   await page.getByLabel('Email or phone').fill('7755994389');
-  await page.getByRole('button', { name: 'Next' }).click();
+  await page.getByLabel('button', { name: 'Next' }).click();
 });
